26f15ea5dcb21ed32ee092429de391b47858036f
fourohfour <fourohfourbukkitdev@gmail.com>
2014-05-14 11:37:40 +0100

    Update DogeHelpBot.py
    Working on comment parsing. Not tested.

--- DogeHelpBot.py
+++ DogeHelpBot.py
@@ -1,35 +1,63 @@
 import praw
 
-r = praw.Reddit(user_agent = "DogeHelpBot - Provides information about Dogecoin on request")
-
-done = []
-user = ""
-passwrd = ""
-subreddits = ""
-
-
-with f as open("./dealtwith.txt"):
-    for line in f:
-        done.append(line)
-
-with f as open("./credentials.txt"):
-    search = 0
-    for line in f:
-        if search == 0:
-            user = line
-            search = 1
-        else:
-            passwrd = line
+def parse(comment):
+    w = comment.body.split()
+    match = ["dogehelpbot", "helpbot"]
+    count = 0
+    nxt = 0
+    for word in w:
+        if word.lower() in match:
+            nxt = count + 1
             break
+        count++
+        
+    if not nxt == 0:
+        try:
+            command = w[nxt]
+        except IndexError:
+            pass
 
-with f as open("./subreddits.txt"):
-    for line in f:
-        subreddits = subreddits + "+" + line
+        if command.lower() == "explain":
+            #Explain
+            
+    
+def main():
+    r = praw.Reddit(user_agent = "DogeHelpBot - Provides information about Dogecoin on request")
 
-r.login(user, passwrd)
+    done = []
+    for i in range(25):
+        done.append(False)
+    user = ""
+    passwrd = ""
+    subreddits = ""
 
-while True:
-    sreddits = r.get_subreddit(subreddits)
-    comments = subreddit.get_comments()
-    flatc = praw.helpers.flatten_tree(comments)
-    #do stuff - TODO
+
+    with f as open("./dealtwith.txt"):
+        for line in f:
+            done.append(line)
+
+    with f as open("./credentials.txt"):
+        search = 0
+        for line in f:
+            if search == 0:
+                user = line
+                search = 1
+            else:
+                passwrd = line
+                break
+
+    with f as open("./subreddits.txt"):
+        for line in f:
+            subreddits = subreddits + "+" + line
+
+    r.login(user, passwrd)
+
+    while True:
+        sreddits = r.get_subreddit(subreddits)
+        comments = subreddit.get_comments()
+        flatc = praw.helpers.flatten_tree(comments)
+        for c in flatc:
+            if c.id not in done:
+                parse(c)
+    
+main()
