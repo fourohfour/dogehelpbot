e24da7df082d5a02749ee5114bee1bdb243081e8
fourohfour <fourohfourbukkitdev@gmail.com>
2014-05-15 06:45:09 +0100

    Small Changes
    Now bootstraps on crash, new pages for mining and wallets.

--- DogeHelpBot.py
+++ DogeHelpBot.py
@@ -18,7 +18,7 @@ def getInfo(phrase):
                     comment.append(info.read() + "  \n")
             except IOError:
                 print("ERROR: NO index.txt FOUND")
-            
+    comment.append("\n  ")       
     comment.append("*Information from DogeHelpBot. More info at /r/DogeHelpBot*")
     return comment
     
@@ -63,52 +63,54 @@ def parse(comment):
             comment.reply(reply)
     
 def main():
-    r = praw.Reddit(user_agent = "DogeHelpBot - Provides information about Dogecoin on request")
+    try:
+        r = praw.Reddit(user_agent = "DogeHelpBot - Provides information about Dogecoin on request")
 
-    done = []
-        
-    user = ""
-    passwrd = ""
-    subreddits = ""
+        done = []
+            
+        user = ""
+        passwrd = ""
+        subreddits = ""
 
 
-    with open("./dealtwith.txt") as f:
-        for line in f:
-            done.append(line.strip())
+        with open("./dealtwith.txt") as f:
+            for line in f:
+                done.append(line.strip())
 
-    with open("./credentials.txt") as f:
-        search = 0
-        for line in f:
-            if search == 0:
-                user = line.strip()
-                search = 1
-            else:
-                passwrd = line.strip()
-                break
-
-    with open("./subreddits.txt") as f:
-        for line in f:
-            subreddits = subreddits + "+" + line.strip()
-
-    r.login(user, passwrd)
-
-    while True:
-        sreddits = r.get_subreddit(subreddits)
-        comments = sreddits.get_comments()
-        for c in comments:
-            if c.id not in done:
-                print("Parsing " + c.id)
-                parse(c)
-                done.insert(0, c.id)
-
-        with open("./dealtwith.txt", "w") as f:
-            count = 0
-            for item in done:
-                if count == 50:
+        with open("./credentials.txt") as f:
+            search = 0
+            for line in f:
+                if search == 0:
+                    user = line.strip()
+                    search = 1
+                else:
+                    passwrd = line.strip()
                     break
-                f.write(item + "\n")
-                count = count + 1
-                
-        time.sleep(8)
 
-main()
+        with open("./subreddits.txt") as f:
+            for line in f:
+                subreddits = subreddits + "+" + line.strip()
+
+        r.login(user, passwrd)
+
+        while True:
+            sreddits = r.get_subreddit(subreddits)
+            comments = sreddits.get_comments()
+            for c in comments:
+                if c.id not in done:
+                    print("Parsing " + c.id)
+                    if not c.author.name == "DogeHelpBot":
+                        parse(c)
+                    done.insert(0, c.id)
+
+            with open("./dealtwith.txt", "w") as f:
+                count = 0
+                for item in done:
+                    if count == 50:
+                        break
+                    f.write(item + "\n")
+                    count = count + 1
+                    
+            time.sleep(8)
+        except:
+            main()
